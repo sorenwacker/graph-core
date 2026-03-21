13c13159edc79d9d118e06c9b0361b6bde7de40a
Sören Wacker <s.wacker@tudelft.nl>
2026-03-21 23:53:07 +0100

    Add ARIA accessibility attributes to key components

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -7,7 +7,7 @@ const emit = defineEmits(['navigate'])
 </script>
 
 <template>
-  <nav class="header-breadcrumbs">
+  <nav class="header-breadcrumbs" aria-label="Breadcrumb navigation">
     <div class="breadcrumb-path">
       <span class="crumb" @click="emit('navigate', -1)">~</span>
       <template v-for="(crumb, index) in breadcrumbs" :key="crumb.id">
